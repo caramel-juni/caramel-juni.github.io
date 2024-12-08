700317fcc28ec3981bfe3f4fc70ce0c4b99b65b9
juniper <juniper@airlockdigital.com>
2024-12-08 16:31:36 +1030

    changes to layout and responsiveness

--- liv-site/script.js
+++ liv-site/script.js
@@ -7,9 +7,8 @@ document.getElementById("login-form").addEventListener("submit", function (event
     const password = document.getElementById("password").value;
   
     // Example validation (Replace this with actual logic)
-    if (username === "admin" && password === "1234") {
-      alert("Login successful!");
-      window.location.href = "welcome.html"; // Redirect to another page
+    if (username === "liv" && password === "daddygeto") {
+      window.location.href = "/liv-site/messages/message_1.html"; // Redirect to another page
     } else {
       document.getElementById("error-message").textContent = "Invalid username or password!";
     }
